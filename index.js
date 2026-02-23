fb9b1cb22a1afce162939b579b699acb6fd9d560
Julian Francis <jgfrancis3@gmail.com>
2026-02-23 16:00:40 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+const employee = {
+    name: "Julian"
+};
+global.employee = employee;
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+  return {
+    ...obj,
+    [key]: value
+  };
+}
+global.updateEmployeeWithKeyAndValue = updateEmployeeWithKeyAndValue;
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+  return obj;
+}
+global.destructivelyUpdateEmployeeWithKeyAndValue = destructivelyUpdateEmployeeWithKeyAndValue;
+function deleteFromEmployeeByKey(obj, key) {
+  const newObj = { ...obj };
+  delete newObj[key];
+  return newObj;
+}
+global.deleteFromEmployeeByKey = deleteFromEmployeeByKey;
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+  delete obj[key];
+  return obj;
+}
+global.destructivelyDeleteFromEmployeeByKey = destructivelyDeleteFromEmployeeByKey;
 // Write your solution in this file!
